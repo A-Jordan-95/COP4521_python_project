6341168d8d8e7f9ae6be54f2722467427fd63de6
A-Jordan-95 <arj14b@my.fsu.edu>
2020-05-27 12:14:50 -0400

    running platformer example w/ open source art

---
+++ EscapeGame.py
@@ -0,0 +1,145 @@
+import arcade
+#import random
+import os
+
+SCREEN_TITLE = "Platformer"
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+
+CHARACTER_SCALING = 0.75
+TILE_SCALING = 0.5
+COIN_SCALING = 0.25
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = (SPRITE_PIXEL_SIZE * TILE_SCALING)
+
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
+LEFT_VIEWPORT_MARGIN = 250
+RIGHT_VIEWPORT_MARGIN = 250
+BOTTOM_VIEWPORT_MARGIN = 100
+TOP_VIEWPORT_MARGIN = 100
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        self.coin_list = None
+        self.wall_list = None
+        self.player_list = None
+        self.player_sprite = None
+        self.physics_engine = None
+        self.view_bottom = 0
+        self.view_left = 0
+        self.score = 0
+        arcade.set_background_color(arcade.color.DIM_GRAY)
+
+    def setup(self):
+        #setup sprite lists
+        self.view_bottom = 0
+        self.view_left = 0
+        self.score = 0
+        self.player_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+
+        #setup player sprite:
+        image_source = "Images/platformChar_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 128
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        #setup map:
+        map_name = "maps/map.tmx"
+        platforms_layer_name = 'Platforms'
+        coins_layer_name = 'Coins'
+        my_map = arcade.tilemap.read_tmx(map_name)
+        #set up platforms:
+        self.wall_list = arcade.tilemap.process_layer(map_object = my_map,
+                                                      layer_name = platforms_layer_name,
+                                                      scaling = TILE_SCALING)
+
+        #setup coins:
+        self.coin_list = arcade.tilemap.process_layer(my_map, coins_layer_name, TILE_SCALING)
+
+        #setup background:
+        if my_map.background_color:
+            arcade.set_background_color(my_map.background_color)
+
+        ##setup physics engine:
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                                             self.wall_list,
+                                                             GRAVITY)
+    def on_draw(self):
+        arcade.start_render()
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.player_list.draw()
+
+        score_text = f"Computer pieces found: {self.score}"
+        arcade.draw_text(score_text, 10 + self.view_left, 10 + self.view_bottom,
+                         arcade.csscolor.WHITE, 18)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.LEFT:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        #update physics engine
+        self.physics_engine.update()
+        #check for coins collected
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                             self.coin_list)
+        #remove found coins and update score:
+        for coin in coin_hit_list:
+            coin.remove_from_sprite_lists()
+            self.score += 1
+
+        #adjust viewport:
+        changed = False
+        #left:
+        left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
+        if self.player_sprite.left < left_boundary:
+            self.view_left -= left_boundary - self.player_sprite.left
+            changed = True
+        #right:
+        right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
+        if self.player_sprite.right > right_boundary:
+            self.view_left += self.player_sprite.right - right_boundary
+            changed = True
+        #up:
+        top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
+        if self.player_sprite.top > top_boundary:
+            self.view_bottom += self.player_sprite.top - top_boundary
+            changed = True
+        #down:
+        bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
+        if self.player_sprite.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+            changed = True
+
+        if changed:
+            self.view_bottom = int(self.view_bottom)
+            self.view_left = int(self.view_left)
+            arcade.set_viewport(self.view_left, SCREEN_WIDTH + self.view_left,
+                                self.view_bottom, SCREEN_HEIGHT + self.view_bottom)
+
+def main():
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
