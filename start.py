5feea2ba48ab00c5d4b1085666de567cc92dee78
ngranger22 <66127325+ngranger22@users.noreply.github.com>
2020-07-29 15:56:58 -0400

    Update start.py

--- start.py
+++ start.py
@@ -189,6 +189,8 @@ class StartView(arcade.View):
     def on_draw(self):
         arcade.start_render()
         arcade.draw_lrwh_rectangle_textured(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        arcade.draw_text("Press 'p' for Pause Menu", SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4 - 25,
+                         arcade.color.BLACK, font_size=15, bold=True, anchor_x="center")
         self.texture1.draw_sized(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 1.3, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 3)
         self.texture2.draw_sized(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 1.7, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 8)
 
